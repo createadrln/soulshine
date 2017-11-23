235376771b7a1800fe3e119f76200aee81732543
Ryan <rmacmurray@unleashed-technologies.com>
2017-11-22 21:07:41 -0500

    HP Updates

--- wp-content/themes/soulshine/page-home.php
+++ wp-content/themes/soulshine/page-home.php
@@ -11,14 +11,13 @@ Template Name: Home Page
     <div class="container_inner-content">
       <div id="inner-content" class="cf inner-content">
         <div id="main" class="m-all cf" role="main">
-          <article id="post-<?php the_ID(); ?>">
-            <section class="main-banner">
-              <div class="main-banner__overlay">
-                <h1 class="page-title">Say something interesting</h1>
-              </div>
-              <img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/42HBC4OR23.jpg" ?>
-            </section>
-          </article>
+          <section class="main-banner" style="background: url('<?php echo get_stylesheet_directory_uri() ?>/library/images/pexels-photo-357501') no-repeat 50% 50%; background-size: 100% auto;">
+            <div class="main-banner__overlay">
+              <h1 class="page-title"><?php echo __('The greatest challenge of the day is: how to bring about a revolution of the heart, a revolution which has to start with each one of us?') ?>
+                <div class="author"><?php echo __('- Dorothy Day') ?></div>
+              </h1>
+            </div>
+          </section>
         </div>
       </div>
     </div>
@@ -26,42 +25,75 @@ Template Name: Home Page
     <section class="home-section__about home-section">
       <div class="container_inner-content">
         <div class="inner-content about m-all wrap cf">
-          <h2 class="section-title">About Soul Shine</h2>
-          <p>It's about breaking free of the working class mentality. It's about living your life for yourself and the people you care most about. It's about finding your own time and space.</p>
-          <p>We help you define and take the steps to get you there.</p>
+          <h2 class="section-title"><?php echo __('About Soul Shine') ?></h2>
+          <p><?php echo __('Wouldn’t it be incredible if everyone took a small step to initiate change in their community? Just imagine the ripple effects of positivity, service, and conscious giving a movement like that would do for the world? Are you ready? The world needs your impact, It’s time to let your Soul Shine!') ?></p>
         </div>
       </div>
     </section>
 
-  </div>
-
-  <section class="home-section__signup home-section">
-    <div class="container_inner-content">
-      <div class="inner-content about m-all wrap cf">
-        <h2 class="section-title">You are almost there!</h2>
-        <!-- Begin MailChimp Signup Form -->
-        <div id="mc_embed_signup">
-          <form action="//rachaelmariefitness.us8.list-manage.com/subscribe/post?u=3ee05c588b003cf445b258d15&amp;id=b388511a70" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <p><label for="mce-EMAIL">Just enter your email below and I will send you my 7 Day Guide to Clean Eating.</label></p>
-            <div class="input-box">
-              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter Email Address" required>
-            </div>
-            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-            <div style="position: absolute; left: -5000px;">
-              <input type="text" name="b_3ee05c588b003cf445b258d15_b388511a70" tabindex="-1" value="">
-            </div>
-            <div class="actions">
-              <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="button">
-                Submit
-              </button>
-            </div>
-            <p class="note">By sending us your email address you are agreeing to receive occasional updates from RachaelMarieFitness.com</p>
-          </form>
+    <section class="home-section__form home-section__form-suggestions home-section">
+      <div class="container_inner-content">
+        <div class="inner-content about m-all wrap cf">
+          <h2 class="section-title"><?php echo __('Get updates from Soul Shine!') ?></h2>
+          <!-- Begin MailChimp Signup Form -->
+          <div id="mc_embed_signup">
+            <form action="//rachaelmariefitness.us8.list-manage.com/subscribe/post?u=3ee05c588b003cf445b258d15&amp;id=b388511a70" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+              <p><label for="mce-EMAIL"><?php echo __('Just enter your email below get email notifications when new podcasts and articles are available!') ?></label></p>
+              <div class="input-box">
+                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="<?php echo __('Enter Email Address') ?>" required>
+              </div>
+              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;">
+                <input type="text" name="b_3ee05c588b003cf445b258d15_b388511a70" tabindex="-1" value="">
+              </div>
+              <div class="actions">
+                <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="button"><?php echo __('Submit') ?></button>
+              </div>
+              <p class="note"><?php echo __('By sending us your email address you are agreeing to receive occasional updates from soulshinerevolution.com') ?></p>
+            </form>
+          </div>
+          <!--End mc_embed_signup-->
         </div>
-        <!--End mc_embed_signup-->
       </div>
-    </div>
-    </div>
-  </section>
+    </section>
+
+    <section class="home-section__form home-section__form-signup home-section">
+      <div class="container_inner-content">
+        <div class="inner-content about m-all wrap cf">
+          <h2 class="section-title"><?php echo __('Podcast Suggestions') ?></h2>
+          <!-- Begin MailChimp Signup Form -->
+          <div id="mc_embed_signup">
+            <form action="//rachaelmariefitness.us8.list-manage.com/subscribe/post?u=3ee05c588b003cf445b258d15&amp;id=b388511a70" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+              <p><label for="mce-EMAIL"><?php echo __('Do you know someone that is making a difference? Please leave a note with some details and contact information.') ?></label></p>
+              <div class="input-box">
+                <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter Email Address" required>
+              </div>
+              <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;">
+                <input type="text" name="b_3ee05c588b003cf445b258d15_b388511a70" tabindex="-1" value="">
+              </div>
+              <div class="actions">
+                <button type="submit" name="subscribe" id="mc-embedded-subscribe" class="button"><?php echo __('Submit') ?></button>
+              </div>
+            </form>
+          </div>
+          <!--End mc_embed_signup-->
+        </div>
+      </div>
+    </section>
+
+    <section class="home-section__about home-section__about-rachael home-section">
+      <div class="m-all wrap cf">
+        <div class="container_inner-content">
+          <div class="inner-content about">
+            <h2 class="section-title">About Rachael</h2>
+            <p>Well hello there! I’m Rachael MacMurray, think of me as your soul sister and that little voice in your ear whispering "you can do it". My mission is to empower the next generation of leaders to step into their greatness and in return find their calling to make the world a better place. I am an entrepreneur, author, activist and all now a podcaster for Soul Shine where we bring you game changers from around the globe to educate, inspire and ignite your soul! Thanks for joining us, let the revolution begin!</p>
+          </div>
+        </div>
+        <img src="<?php echo get_stylesheet_directory_uri() ?>/library/images/rachael2.jpg" />
+      </div>
+    </section>
+
+  </div>
 
 <?php get_footer(); ?>
